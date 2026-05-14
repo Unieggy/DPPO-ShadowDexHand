6e446999422f829f6f03101fdfdc21ca7edc33c3
Unieggy <uniegg132@gmail.com>
2026-05-14 15:00:44 -0700

    added behavior cloning to pretrain the diffusion model

---
+++ train.py
@@ -0,0 +1,59 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utiles.data import DataLoader,TensorDataset
+
+def train_behavior_cloning(actor,diffusion_scheduler,expert_states,expert_action_chunks,K:int,epochs:int,batch_size:int,device="cpu"):
+    """
+    Supervised learning pretraining to train the actor to mimic expert demonstrations before RL
+    Uses a ddpm's forward noise to jump instantly to random diffusion steps k 
+    and corrupt perfect human data with Gaussian noise. 
+    The network learns to guess the noise added
+    """
+
+    actor.train()
+    optimizer=optim.AdamW(actor.parameters(),lr=1e-4)
+    loss_fn=nn.MSELoss()
+
+    #pack expert data into pytorch dataloader
+    #expert state:[total_samples,obs_dim]
+    #expert_action_chunks:[total_samples,chunk_size,act_dim]
+    dataset=TensorDataset(expert_states,expert_action_chunks)
+    dataloader=DataLoader(dataset,batch_size=batch_size,shuffle=True)
+
+    for epoch in range(epochs):
+        for batch_states,batch_actions in dataloader:
+            batch_size=batch_states.to(device) #(batch_size,obs_dim)
+            batch_actions=batch_actions.to(device)#(batch_size,chunk_size,act_dim)
+
+            #1 randomize diffusion steps k
+            #we train the network to denoise from any step in the sequence
+            #random_ks:[batch_size]
+            random_ks=torch.randin(0,K,(batch_size,),device=device,dtype=torch.long)
+
+
+            #2 generate pure gaussian noise
+            #pure noise:[batch_size,chunk_size,act_dim]
+            pure_noise=torch.randn_like(batch_actions)
+
+            #3 create the trianing target
+            #mathmatically corruput the expert action with the pure noise to simulate step k
+            #noist_actions :[batch_size,chunk_size,act_dim]
+            #hugging face diffuser, not a neural network just a strict mathmatical formulas to do the noise adding
+            #built in method to do forward diffusion, actor is our diffusion mlp defined in network
+            
+            noisy_actions=diffusion_scheduler.add_noise(batch_actions,pure_noise,random_ks)
+
+            #4 neural network forward pass
+            #actor predicts what noise was added to the clean action
+            #predicted_noise: [batch_size,chunk_size,act_dim]
+            predicted_noise=actor(batch_states,noisy_actions,random_ks)
+
+            # 5 mse loss
+            loss=loss_fn(predicted_noise,pure_noise)
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+    return actor
+
